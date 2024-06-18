a2c19bf59b0db0ad923f104ca1bb384d98a4a3bf
Elijah Adams <emadams92@gmail.com>
2024-06-17 18:56:47 -0700

    Add node server

---
+++ server/index.cjs
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path')
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.static(path.join(__dirname, '../dist')));
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, '..dist', 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+  console.log(`http://127.0.0.1:${PORT}/`)
+});
